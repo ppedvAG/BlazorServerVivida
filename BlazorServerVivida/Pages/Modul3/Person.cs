e47bd6a78b62abc358a996a80304189fa73abee4
Hannes Preishuber <hannesp@ppedv.de>
2022-09-01 14:09:44 +0200

    Viewmodel Konzept Ideen

---
+++ BlazorServerVivida/Pages/Modul3/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServerVivida.Pages.Modul3
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        void save()
+        { }
+
+    }
+}
