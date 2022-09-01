9c43cb871e519b4a82b3f7526b7b3c05553bade1
Hannes Preishuber <hannesp@ppedv.de>
2022-09-01 11:29:20 +0200

    Routing Beispiele

---
+++ BlazorServerVivida/Pages/Modul2/Hannes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorServerVivida.Pages.Modul2
+{
+    public class HannesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
