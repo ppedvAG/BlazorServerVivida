d545730b7233d8148a53421b2251121fde344edd
Hannes Preishuber <hannesp@ppedv.de>
2022-09-01 16:12:38 +0200

    Rest Call auf TODO

---
+++ BlazorServerVivida/Pages/Modul4/ToDos.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServerVivida.Pages.Modul4
+{
+    public class ToDos
+    {
+   
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
