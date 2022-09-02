f60b5f99768a3ccb089a2fdbad54aaf7c127b8ff
Hannes Preishuber <hannesp@ppedv.de>
2022-09-02 09:41:56 +0200

    HTML Form Grundlagen

---
+++ BlazorServerVivida/Pages/Modul5/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorServerVivida.Pages.Modul5
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
