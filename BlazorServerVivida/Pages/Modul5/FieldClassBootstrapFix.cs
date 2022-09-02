c2814453789fab7bf582355437cd9fc5ff40848f
Hannes Preishuber <hannesp@ppedv.de>
2022-09-02 10:30:51 +0200

    Fix für Css Bootstrap is-valid

---
+++ BlazorServerVivida/Pages/Modul5/FieldClassBootstrapFix.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorServerVivida.Pages.Modul5
+{
+    public class FieldClassBootstrapFix : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isinvalid = editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isinvalid ? "is-invalid":"is-valid";
+           
+
+        }
+    }
+}
