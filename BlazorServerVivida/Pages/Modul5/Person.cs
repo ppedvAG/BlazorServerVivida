3f8df2938e409b2e6ed8dc9488704fa913108d50
Hannes Preishuber <hannesp@ppedv.de>
2022-09-02 10:24:18 +0200

    Editform Valdierung

---
+++ BlazorServerVivida/Pages/Modul5/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerVivida.Pages.Modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="Muss was rein")]
+        [StringLength(10, ErrorMessage="max 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Muss was rein")]
+        [Range(120,190, ErrorMessage="zwischen 120 und 190")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "Muss was rein")]
+        public DateTime GebTag { get; set; }
+    }
+}
